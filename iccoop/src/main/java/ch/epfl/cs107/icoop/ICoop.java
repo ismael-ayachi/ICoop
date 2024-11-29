43559e7f6c0b63bb630a11e45f6bffabb943bf6e
ismaelayachi <ismael.ayachi@epfl.ch>
2024-11-29 13:32:33 +0100

    Initial commit

---
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/ICoop.java
@@ -0,0 +1,13 @@
+package ch.epfl.cs107.icoop;
+
+
+import ch.epfl.cs107.play.areagame.AreaGame;
+
+
+public class ICoop extends AreaGame {
+    // TO BE COMPLETED
+    @Override
+    public String getTitle() {
+        return "ICoop";
+    }
+}
